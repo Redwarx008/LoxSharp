f8a6f1d81e1fa8361f6fa8ea7eb758287329fda2
Redwarx008 <redwarx008@outlook.com>
2023-06-28 18:24:06 +0800

    init

---
+++ LoxSharp.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LoxSharp.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
